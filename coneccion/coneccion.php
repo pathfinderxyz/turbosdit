ac8b0094e018ad3917c439dfc1249c98ea97952f
jose <josemcarvajal4@gmail.com>
2022-03-04 09:19:02 -0400

    conexion nueva

--- coneccion/coneccion.php
+++ coneccion/coneccion.php
@@ -1,9 +1,9 @@
 <?php  
-	$user = 'postgres';
-	$passwd = '';
-	$db = 'turbos';
+	$user = 'amaszjpzoroxuv';
+	$passwd = '0d97022743dce2876808c13b377f0b27ce49ff07edb7256cb5741fc8222325bb';
+	$db = 'da6ojti9kjs5p8';
 	$port = 5432;
-	$host = 'localhost';
+	$host = 'ec2-34-206-148-196.compute-1.amazonaws.com';
 	$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
 	$cnx = pg_connect($strCnx) or die ("Error de conexion. ". pg_last_error());
 
